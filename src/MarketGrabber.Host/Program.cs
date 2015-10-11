31c99b0ef0061a4c09fbf985909c025ad3b945aa
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2015-10-11 19:32:03 +0300

    VS solution added

---
+++ src/MarketGrabber.Host/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketGrabber.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("\t\t\t   Hello Market Grabber!");
+            Console.ReadKey();
+        }
+    }
+}
