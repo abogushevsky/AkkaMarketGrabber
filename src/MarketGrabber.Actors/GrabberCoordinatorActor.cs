a46b118c510fafa7032896f7f6a22954dfd76935
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2015-10-13 09:26:46 +0300

    Child actors created

--- src/MarketGrabber.Actors/GrabberCoordinatorActor.cs
+++ src/MarketGrabber.Actors/GrabberCoordinatorActor.cs
@@ -1,11 +1,24 @@
 ﻿using Akka.Actor;
+using Akka.Routing;
 
 namespace MarketGrabber.Actors
 {
     public class GrabberCoordinatorActor : ReceiveActor
     {
+        private IActorRef downloaderActor;
+        private IActorRef parserActor;
+
         public GrabberCoordinatorActor()
         {
+            Waiting();
+        }
+
+        protected override void PreStart()
+        {
+            this.downloaderActor =
+                Context.ActorOf(Props.Create(() => new WebGrabberActor()).WithRouter(new SmallestMailboxPool(10)));
+            this.parserActor =
+                Context.ActorOf(Props.Create(() => new HtmlParserActor()).WithRouter(new RoundRobinPool(10)));
         }
 
         private void Waiting()
