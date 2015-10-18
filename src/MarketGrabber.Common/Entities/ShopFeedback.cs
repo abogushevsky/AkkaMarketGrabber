875905995b3201db92e30639821d5d5d2ee77660
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2015-10-18 17:15:51 +0300

    ShopFeedback entity added

---
+++ src/MarketGrabber.Common/Entities/ShopFeedback.cs
@@ -0,0 +1,25 @@
+﻿namespace MarketGrabber.Common.Entities
+{
+    /// <summary>
+    /// Class represents single feedback for shop
+    /// </summary>
+    public class ShopFeedback : Feedback
+    {
+        public ShopFeedback(Shop shop, Feedback feedback)
+            : this(shop, feedback.Stars, feedback.Positive, feedback.Negative, feedback.Description)
+        {
+            
+        }
+
+        public ShopFeedback(Shop shop, int stars, string positive, string negative, string description) 
+            : base(stars, positive, negative, description)
+        {
+            this.Shop = shop;
+        }
+
+        /// <summary>
+        /// The store whitch feedback was written for
+        /// </summary>
+        public Shop Shop { get; }
+    }
+}
