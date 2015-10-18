65ae8bffdfa1f2243670dfcb8fd603fac2a53c73
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2015-10-18 18:31:56 +0300

    Missed files

---
+++ src/MarketGrabber.Actors/Messages/ParseMessageBase.cs
@@ -0,0 +1,12 @@
+﻿namespace MarketGrabber.Actors.Messages
+{
+    public abstract class ParseMessageBase
+    {
+        public ParseMessageBase(string content)
+        {
+            this.Content = content;
+        }
+
+        public string Content { get; } 
+    }
+}
---
+++ src/MarketGrabber.Common/Entities/Feedback.cs
@@ -0,0 +1,36 @@
+﻿namespace MarketGrabber.Common.Entities
+{
+    /// <summary>
+    /// Class represents single feedback
+    /// </summary>
+    public class Feedback
+    {
+        public Feedback(int stars, string positive, string negative, string description)
+        {
+            this.Stars = stars;
+            this.Positive = positive;
+            this.Negative = negative;
+            this.Description = description;
+        }
+
+        /// <summary>
+        /// Stars count
+        /// </summary>
+        public int Stars { get; }
+
+        /// <summary>
+        /// Positive part of feedback
+        /// </summary>
+        public string Positive { get; }
+
+        /// <summary>
+        /// Negative part of feedback
+        /// </summary>
+        public string Negative { get; }
+
+        /// <summary>
+        /// Some users desciption
+        /// </summary>
+        public string Description { get; }
+    }
+}
